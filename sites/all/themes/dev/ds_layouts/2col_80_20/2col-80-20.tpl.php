a71bcc79c1b3499a36a618d945ef1c16e5e4a9c0
budnyjj <budnyjj@gmail.com>
2012-08-09 16:04:38 +0300

    added pathauto

---
+++ sites/all/themes/dev/ds_layouts/2col_80_20/2col-80-20.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Custom Display Suite 2 column stacked template.
+ */
+?>
+<div class="ds-2col-stacked <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php if ($left): ?>
+    <div class="group-left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div class="group-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </div>
+  <?php endif; ?>
+
+</div>
