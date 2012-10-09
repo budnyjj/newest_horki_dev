9219597de28f622e133b0785ef1a82325606aa0c
horki server <horkiinf@host385.hostmonster.com>
2012-10-09 13:28:09 -0600

    push from repo

--- databases/unix.dump.sh
+++ databases/unix.dump.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-cd /home/public_html/www/horki
+drush sql-dump --gzip > ./NEWEST-HORKI-DEV-`date '+%m-%d-%H-%M'`.sql.gz
 
-drush sql-dump --gzip > databases/NEWEST-HORKI-DEV-`date '+%m-%d-%H-%M'`.sql.gz
