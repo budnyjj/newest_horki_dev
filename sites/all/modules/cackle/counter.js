241c1343084f19a4e23a0a507025b7a9840ee4b8
budnyjj <budnyjj@gmail.com>
2013-07-26 18:35:52 +0300

    deleted cackle

--- sites/all/modules/cackle/counter.js
+++
@@ -1,8 +0,0 @@
-var mcSite = Drupal.settings.cackle.cackle_mc_site;
-(function() {
-    var mc = document.createElement('script');
-    mc.type = 'text/javascript';
-    mc.async = true;
-    mc.src = 'http://cackle.me/mc.count-min.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(mc);
-})();
