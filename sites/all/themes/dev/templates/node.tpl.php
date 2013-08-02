3768dd178f10cfd33ec12f404aa5d9a58455e7a4
HORKI.INFO SRV <admin@horki.info>
2013-08-02 09:14:48 -0600

    deleted addthis

--- sites/all/themes/dev/templates/node.tpl.php
+++ sites/all/themes/dev/templates/node.tpl.php
@@ -27,23 +27,7 @@
 	
 	if($view_mode != 'teaser') {
 	if (!empty($content['links'])): ?>
-<div id="socsetki">	
-	   <p><strong>ПАДЗЯЛІЦЦА:</strong></p>	
-	
-<!-- AddThis Button BEGIN -->
-<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-<a class="addthis_button_odnoklassniki_ru"></a>
-<a class="addthis_button_vk"></a>
-<a class="addthis_button_facebook"></a>
-<a class="addthis_button_twitter"></a>
-<a class="addthis_button_google_plusone_share"></a>
-<a class="addthis_button_email"></a>
-<a class="addthis_button_print"></a>
-<a class="addthis_button_favorites"></a>
-</div>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=undefined"></script>
-<!-- AddThis Button END -->
-</div>      
+  
 	  <nav class="links node-links clearfix">
 	  
 	  <?php print render($content['links']); ?></nav>
