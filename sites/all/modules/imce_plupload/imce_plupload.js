abcc0b44483a631712c9033758da778ad1b45d0d
budnyjj <budnyjj@gmail.com>
2013-08-10 18:20:17 +0300

    up to 7.23

--- sites/all/modules/imce_plupload/imce_plupload.js
+++ sites/all/modules/imce_plupload/imce_plupload.js
@@ -11,9 +11,30 @@ imce.uploadValidate = function (data, form, options) {
       }
     }
     var sep = path.indexOf('/') == -1 ? '\\' : '/';
-    options.url = imce.ajaxURL('upload'); //make url contain current dir.
+    // Make url contain current dir
+    options.url = imce.ajaxURL('upload');
     imce.fopLoading('upload', true);
   });
   
   return true;
 };
+
+/**
+ * Resets the upload form after a succesful upload 
+ */
+jQuery(document).ready(function() {
+  var uploader = jQuery('.plupload-element').pluploadQueue();
+  
+  uploader.bind('UploadComplete', function() {
+    if (uploader.total.uploaded == uploader.files.length) {
+      jQuery(".plupload_buttons").css("display", "inline");
+      jQuery(".plupload_upload_status").css("display", "inline");
+      
+      // Give IMCE some time to finish the uploading.
+      setTimeout(function () {
+        uploader.splice();
+      }, 200);
+    }
+  });
+  
+});
