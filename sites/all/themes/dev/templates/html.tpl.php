ce51fc5a291b106d516beaca98883bc1b3ff0153
live site <horki@akara.idhun.by.vps.neolocation.net>
2014-03-24 00:38:52 +0300

    fix jquery not found error

--- sites/all/themes/dev/templates/html.tpl.php
+++ sites/all/themes/dev/templates/html.tpl.php
@@ -26,9 +26,9 @@
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
-  <?php print $page_bottom; ?>
-
+  
   <?php print $scripts; ?>
+  <?php print $page_bottom; ?>
 
   <!-- Yandex.Metrika counter -->
   <script type="text/javascript">
