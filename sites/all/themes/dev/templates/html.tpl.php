e6834346b42f13a35284d16425c072ae69b3c39a
horki.info server <admin@new.horki.info>
2015-06-07 11:39:02 +0300

    add rich favicon support

--- sites/all/themes/dev/templates/html.tpl.php
+++ sites/all/themes/dev/templates/html.tpl.php
@@ -3,7 +3,26 @@
 <head<?php print $rdf->profile; ?>>
 <?php print $head; ?>
 <title><?php print $head_title; ?></title>  
-<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="icon" sizes="16x16 32x32 64x64" href="/favicon.ico">
+<link rel="icon" type="image/png" sizes="196x196" href="/sites/default/files/favicons/favicon-192.png">
+<link rel="icon" type="image/png" sizes="160x160" href="/sites/default/files/favicons/favicon-160.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/sites/default/files/favicons/favicon-96.png">
+<link rel="icon" type="image/png" sizes="64x64" href="/sites/default/files/favicons/favicon-64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/sites/default/files/favicons/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/sites/default/files/favicons/favicon-16.png">
+<link rel="apple-touch-icon" href="/sites/default/files/favicons/favicon-57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/sites/default/files/favicons/favicon-114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/sites/default/files/favicons/favicon-72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/sites/default/files/favicons/favicon-144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/sites/default/files/favicons/favicon-60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/sites/default/files/favicons/favicon-120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/sites/default/files/favicons/favicon-76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/sites/default/files/favicons/favicon-152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/sites/default/files/favicons/favicon-180.png">
+<meta name="msapplication-TileColor" content="#FFFFFF">
+<meta name="msapplication-TileImage" content="/sites/default/files/favicons/favicon-144.png">
+<meta name="msapplication-config" content="/sites/default/files/favicons/browserconfig.xml">  
 <?php print $styles; ?>
 
 <meta name="verify-v1" content="A29kVzxMcO/zSJlO3EeJzU6vnTgF5KY1HcgHqId0+qA=" />
