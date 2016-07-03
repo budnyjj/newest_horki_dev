e0435c770f825d906fcd452172497a1daaf84eca
horki.info server <admin@new.horki.info>
2016-07-03 15:39:52 +0300

    update Drupal to 7.44

--- sites/all/themes/dev/templates/html.tpl.php
+++ sites/all/themes/dev/templates/html.tpl.php
@@ -35,6 +35,7 @@
 <script type="text/javascript">
 <!--//--><![CDATA[//><!--
 var pp_gemius_identifier = '.FuVJrfD0SLzrGxzLutoYre976b8joLyu6XDUVRV1vD.m7';
+// lines below shouldn't be edited
 function gemius_pending(i) { window[i] = window[i] || function() {var x = window[i+'_pdata'] = window[i+'_pdata'] || []; x[x.length]=arguments;};};
 gemius_pending('gemius_hit'); gemius_pending('gemius_event'); gemius_pending('pp_gemius_hit'); gemius_pending('pp_gemius_event');
 (function(d,t) {try {var gt=d.createElement(t),s=d.getElementsByTagName(t)[0],l='http'+((location.protocol=='https:')?'s':''); gt.setAttribute('async','async');
