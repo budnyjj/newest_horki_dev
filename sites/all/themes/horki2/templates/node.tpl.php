142b30c997977175e77bf86cb1a916095a38016b
horki.info server <admin@new.horki.info>
2017-02-19 23:54:46 +0300

    horki2: node: actions

--- sites/all/themes/horki2/templates/node.tpl.php
+++ sites/all/themes/horki2/templates/node.tpl.php
@@ -30,14 +30,4 @@
       print render($content);
     ?>
   </div>
-
-  <div class="clearfix">
-    <?php if($view_mode != 'teaser') {
-	if (!empty($content['links'])): ?> 
-	  <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
-    <?php endif; ?>
-	 <?php }; ?>
-
-    <?php print render($content['comments']); ?>
-  </div>
 </article>
