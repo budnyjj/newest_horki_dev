394ebea765b2eec4caa78b03cec223caebd56e57
horki.info server <admin@new.horki.info>
2019-10-06 10:27:01 +0300

    updated Views RSS submodules

--- sites/all/themes/horki2/templates/views-view-views-rss--rss-yandex-turbo.tpl.php
+++ sites/all/themes/horki2/templates/views-view-views-rss--rss-yandex-turbo.tpl.php
@@ -4,5 +4,5 @@
  * Default template for feed displays that use the RSS Fields style.
  */
 ?>
-<?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
+<?php print "<?xml"; ?> version="1.0" encoding="UTF-8" <?php print "?>"; ?>
 <?php print str_replace('<item>', '<item turbo="true">', $rss_feed); ?>
